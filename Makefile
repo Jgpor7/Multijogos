637f907b235a0dec4457af09477d16cbe0125112
jgpor <jgpor@hwc15.cin.ufpe.br>
2026-05-12 16:06:31 -0300

    mecanicas adicionadas

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 CXX = g++
 CXXFLAGS = -Wall -std=c++17
 
-# 1. Diga ao compilador para procurar headers na sua pasta
-INCLUDES = -I./lib
+# 1. Ajustado para a pasta 'raylib'
+INCLUDES = -I./raylib
 
-# 2. Diga ao linker para procurar a biblioteca (.a) na sua pasta
-LDFLAGS = -L./lib
+# 2. Ajustado para a pasta 'raylib'
+LDFLAGS = -L./raylib
 
 TARGET = multijogos
 SRCS = $(wildcard *.cpp)
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <random>
 
+#include <unordered_map> // adicionando os dicionarios hash
+
 //#define LARGURATELA 854
 //#define ALTURATELA 480
 
@@ -10,53 +12,66 @@
 #define ALTURATELA (float)GetScreenHeight()
 
 #define RAIOSPAWNZUMBI ALTURATELA/3
+#define RAIOMIRA 150
+
+#define PI 3.1415
 
 int calcularRestoPositivo(int a, int b){
     return ((a % b) + b) % b;
 }
 
+float converterRadianoGraus(float rad){
+    return (rad*180)/PI;
+}
+
 int randomInt(int a, int b);
+float det2x2(Vector2 a, Vector2 b);
+
+Vector2 calcularModulo(Vector2 v){
+    float modulo = sqrt(pow(v.x, 2) + pow(v.y, 2));
+    return (Vector2){v.x/modulo, v.y/modulo};
+}
 
 class Zumbi{
 
     public:
 
-    Zumbi(float velocidadeIn, int vidaIn, Vector2 posicaoJogador){
-        velocidade = velocidadeIn;
-        vida = vidaIn;
+        Zumbi(float velocidadeIn, int vidaIn, Vector2 posicaoJogador){
+            velocidade = velocidadeIn;
+            vida = vidaIn;
 
-        // gerando nascimento aleatorio
-        bool lugar_valido = false;
-        while(!lugar_valido){ // gerando o zumbi em um raio distante do jogador
-            posicao.x = randomInt(0, LARGURATELA);
-            posicao.y = randomInt(0, ALTURATELA);
+            // gerando nascimento aleatorio
+            bool lugar_valido = false;
+            while(!lugar_valido){ // gerando o zumbi em um raio distante do jogador
+                posicao.x = randomInt(0, LARGURATELA);
+                posicao.y = randomInt(0, ALTURATELA);
 
-            int posicao_x_relativa = (int)(posicao.x - posicaoJogador.x);
-            int posicao_y_relativa = (int)(posicao.y - posicaoJogador.y);
+                int posicao_x_relativa = (int)(posicao.x - posicaoJogador.x);
+                int posicao_y_relativa = (int)(posicao.y - posicaoJogador.y);
 
-            if(sqrt(pow(posicao_x_relativa, 2) + pow(posicao_y_relativa, 2)) > RAIOSPAWNZUMBI) lugar_valido = true;
+                if(sqrt(pow(posicao_x_relativa, 2) + pow(posicao_y_relativa, 2)) > RAIOSPAWNZUMBI) lugar_valido = true;
+            }
+
+            retanguloZumbi = {posicao.x, posicao.y, ALTURATELA/20, LARGURATELA/17};
         }
 
-        retanguloZumbi = {posicao.x, posicao.y, ALTURATELA/20, LARGURATELA/17};
-    }
+        void desenharZumbi(){
+            DrawRectangleRec(retanguloZumbi, RED);
+        }
 
-    void desenharZumbi(){
-        DrawRectangleRec(retanguloZumbi, RED);
-    }
+        Vector2 retornarPosicao(){
+            return posicao;
+        }
 
-    Vector2 retornarPosicao(){
-        return posicao;
-    }
-
-    void seguirJogador(Vector2 posicaoJogador){
-        Vector2 vetorVisado = {posicaoJogador.x - posicao.x, posicaoJogador.y - posicao.y};
-        float moduloVetor = sqrt(pow(vetorVisado.x, 2) + pow(vetorVisado.y, 2));
-        vetorVisado = {(velocidade * vetorVisado.x)/moduloVetor, (velocidade * vetorVisado.y)/moduloVetor};
-        posicao.x = posicao.x + vetorVisado.x;
-        posicao.y = posicao.y + vetorVisado.y;
-        retanguloZumbi.x = posicao.x;
-        retanguloZumbi.y = posicao.y;
-    }
+        void seguirJogador(Vector2 posicaoJogador){
+            Vector2 vetorVisado = {posicaoJogador.x - posicao.x, posicaoJogador.y - posicao.y};
+            float moduloVetor = sqrt(pow(vetorVisado.x, 2) + pow(vetorVisado.y, 2));
+            vetorVisado = {(velocidade * vetorVisado.x)/moduloVetor, (velocidade * vetorVisado.y)/moduloVetor};
+            posicao.x = posicao.x + vetorVisado.x;
+            posicao.y = posicao.y + vetorVisado.y;
+            retanguloZumbi.x = posicao.x;
+            retanguloZumbi.y = posicao.y;
+        }
 
     private:
         float velocidade;
@@ -74,7 +89,9 @@ class jogador{
         vida = 5;
         balas = 20;
         posicao = {LARGURATELA/2, ALTURATELA/2};
-        retanguloJogador = {posicao.x, posicao.y, ALTURATELA/20, LARGURATELA/17};
+        retanguloJogador = {posicao.x - ALTURATELA/40, posicao.y - ALTURATELA/40, ALTURATELA/20, ALTURATELA/20};
+        Texture2D mira = LoadTexture("resources/setavermelha.png");
+        texturas["mira"] = mira;
     }
 
     Vector2 retornarPosicao(){
@@ -83,6 +100,7 @@ class jogador{
 
     void desenharJogador(){
         DrawRectangleRec(retanguloJogador, GREEN);
+        DrawText("X", posicao.x, posicao.y, 10, RED);
     }
 
     void moverJogador(){
@@ -105,32 +123,35 @@ class jogador{
     void atualizarPosicao(){
         posicao.x = calcularRestoPositivo((int)posicao.x, LARGURATELA);
         posicao.y = calcularRestoPositivo((int)posicao.y, ALTURATELA);
-        retanguloJogador.x = posicao.x;
-        retanguloJogador.y = posicao.y;
+        retanguloJogador.x = posicao.x - ALTURATELA/40;
+        retanguloJogador.y = posicao.y - ALTURATELA/40;
     }
 
     void gerarMira(){
 
-        Texture2D textura = LoadTexture("resources/setavermelha.png");
-
         Vector2 posicaoMouse = GetMousePosition();
+        Vector2 vetorBase = {1,0};
+        Vector2 vetorDirecao = {posicaoMouse.x -posicao.x, posicaoMouse.y - posicao.y};
+        
+        float angulorad = std::atan2(det2x2(vetorBase, vetorDirecao), vetorBase.x*vetorDirecao.x + vetorBase.y*vetorDirecao.y); // θ=atan2(ux​vy​−uy​vx​,ux​vx​+uy​vy​)
 
-        float tangente = (posicaoMouse.y - posicao.y)/(posicaoMouse.x - posicaoMouse.x);
+        float angulo = converterRadianoGraus(angulorad) + 90;
 
+        if(angulo <0) angulo += 360;
 
-        Vector2 centro = { (float)textura.width / 2.0f, (float)textura.height / 2.0f };
+        Vector2 centroImagem = {posicao.x + RAIOMIRA*std::cos(angulorad), posicao.y + RAIOMIRA*std::sin(angulorad)};
+
+        Vector2 centro = { (float)texturas["mira"].width / 2.0f, (float)texturas["mira"].height / 2.0f };
 
             // RETÂNGULO DE ORIGEM: Pega a imagem inteira (do pixel 0,0 até a largura/altura total)
-            Rectangle sourceRec = { 0.0f, 0.0f, (float)textura.width, (float)textura.height };
+            Rectangle sourceRec = { 0.0f, 0.0f, (float)texturas["mira"].width, (float)texturas["mira"].height };
 
             // RETÂNGULO DE DESTINO: Define onde na tela e qual o tamanho final
             // Nota: x e y aqui serão onde o CENTRO da imagem vai ficar posicionado.
-            Rectangle destRec = { posicao.x, posicao.y, (float)textura.width, (float)textura.height };
+            Rectangle destRec = { centroImagem.x, centroImagem.y, (float)texturas["mira"].width, (float)texturas["mira"].height };
 
             // Desenha a imagem completa rotacionando no centro
-            DrawTexturePro(textura, sourceRec, destRec, centro, 180, WHITE);
-
-        UnloadTexture(textura);
+            DrawTexturePro(texturas["mira"], sourceRec, destRec, centro, angulo, WHITE);
         
     }
 
@@ -140,8 +161,66 @@ class jogador{
         int balas;
         Rectangle retanguloJogador;
         Vector2 posicao;
+        std::unordered_map<std::string, Texture2D> texturas; //  hash de texturas
 };
 
+class Arma{
+    public:
+
+        Arma(std::string n){
+            nome = n;
+
+            if(nome == "G18"){
+                qtdMunicao = 6; dano = 1;  peso = 0.3f; frequencia = 1; velocidade = 3;
+            } else if(nome == "AK47"){
+                qtdMunicao = 24; dano = 3; peso = 2; frequencia = 0.1f; velocidade = 5;
+            }
+        }
+
+        void atirar(Vector2 posicaoJogador){ // diminui a munição e joga na lista de projeteis
+            qtdMunicao--;
+            Vector2 posicaomouse = GetMousePosition();
+            Vector2 vetordirecao = {posicaomouse.x - posicaoJogador.x, posicaomouse.y - posicaoJogador.y};
+            vetordirecao = calcularModulo(vetordirecao);
+            Projetil p = {{posicaoJogador.x, posicaoJogador.y, 20, 20}, vetordirecao, dano};
+            listaProjeteis.push_back(p);
+        }
+
+        std::vector<int> percorrerProjeteis(std::vector<Zumbi> listaZumbis){
+            std::vector<int> indices;
+
+            for(int i = 0; i < listaProjeteis.size(); i++){
+                Projetil projetil = listaProjeteis[i];
+                DrawRectangleRec(projetil.retangulo, BLUE);
+
+                for(int n = 0; n < listaZumbis.size(); n++){
+                    //if()
+                }
+
+                projetil.retangulo.x += projetil.direcao.x*velocidade;
+                projetil.retangulo.y += projetil.direcao.y*velocidade;                
+            }
+
+            return indices;
+        }
+
+    private:
+        std::string nome;
+        int qtdMunicao;
+        int dano;
+        float peso;
+        float frequencia;
+        float velocidade;
+    
+        typedef struct Projetil{
+            Rectangle retangulo;
+            Vector2 direcao;
+            int danoProjetil;
+        };
+        
+        std::vector<Projetil> listaProjeteis;
+
+};
 int main(){
 
     // --- 1. Inicialização ---
@@ -173,7 +252,8 @@ int main(){
         
         BeginDrawing();
 
-            ClearBackground(BLACK);
+            ClearBackground(WHITE);
+            player.gerarMira();
 
             
             for(int i = 0; i < listaZumbis.size(); i++) {
@@ -184,8 +264,7 @@ int main(){
             DrawText(TextFormat("x: %.2f\ny: %.2f", player.retornarPosicao().x, player.retornarPosicao().y), 700, 50, 20, WHITE);
 
             player.desenharJogador();
-            player.gerarMira();
-    
+            
         EndDrawing();
 
         player.moverJogador();
@@ -207,4 +286,8 @@ int randomInt(int a, int b){
 
     std::uniform_int_distribution<> distr(a, b);
     return distr(gen);
+}
+
+float det2x2(Vector2 a, Vector2 b){
+    return a.x*b.y - a.y*b.x;
 }
